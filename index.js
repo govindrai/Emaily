22cc411a163f31ef018fd9a056d452a9fc9d23a4
Govind Rai <raigovind93@gmail.com>
2017-08-02 22:07:42 -0700

    Setup mLab mongodb connection using moongose orm

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 // Modules
 const express = require("express");
+const mongoose = require("mongoose");
+const mongoURI = require("./config/keys").mongoURI;
 
 // Services
 require("./services/passport");
+mongoose.connect(mongoURI, { useMongoClient: true });
 
 const app = express();
 

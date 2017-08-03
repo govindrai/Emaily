f536b849eb78e0463791c03c0dfb10ff7af3f457
Govind Rai <raigovind93@gmail.com>
2017-08-02 23:47:35 -0700

    Add User model and add logic to add user to database upon signup

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 // Modules
 const express = require("express");
 const mongoose = require("mongoose");
+
+// Configurations
 const mongoURI = require("./config/keys").mongoURI;
+
+// Models
+require("./models/User");
 
 // Services
 require("./services/passport");
+
 mongoose.connect(mongoURI, { useMongoClient: true });
 
 const app = express();
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,13 @@
+// Modules
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
+
+// Configurations
 const keys = require("../config/keys");
+
+// Models
+const User = mongoose.model("users");
 
 passport.use(
   new GoogleStrategy(
@@ -10,9 +17,14 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log("access token", accessToken);
-      console.log("refresh token", refreshToken);
-      console.log("profile", profile);
+      User.findOne({ googleId: profile.id }).then(user => {
+        if (!user) {
+          new User({ googleId: profile.id }).save().then(user => {
+            done(null, user);
+          });
+        }
+        done(null, user);
+      });
     }
   )
 );

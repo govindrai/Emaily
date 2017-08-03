cef853cf8c85e4f38dc8aa84b4e3216a207d08cf
Govind Rai <raigovind93@gmail.com>
2017-08-03 00:26:20 -0700

    Enable session management in express and enable sessions for passport.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // Modules
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 
 // Configurations
-const mongoURI = require("./config/keys").mongoURI;
+const { mongoURI, cookieKey } = require("./config/keys");
 
 // Models
 require("./models/User");
@@ -14,6 +16,16 @@ require("./services/passport");
 mongoose.connect(mongoURI, { useMongoClient: true });
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Routes
 require("./routes/auth")(app);
